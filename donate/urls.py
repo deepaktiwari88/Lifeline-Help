f0731effdb8cbc71c214bebd71d99afedef519b9
Madhav Pruthi <madhavpruthi0@gmail.com>
2018-05-29 00:29:47 +0530

    addition

--- donate/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from .views import index,confirm
-
-app_name = "donate"
-
-urlpatterns = [
-    url(r'^$', index , name='index'),
-    url(r'^confirm/$', confirm, name='confirm')
-]
